e4c97f215327822048dda8e6dd6638f63e8c2b12
AnastasiyaRushanskaya <nastenka.rus@gmail.com>
2022-11-15 11:38:57 +0100

    changes added

--- index.js
+++ index.js
@@ -10,44 +10,47 @@ function getUserName() {
 }
 
 function setMaxDate() {
+  let datePickerId = document.getElementById('datePickerId');
   datePickerId.max = new Date().toISOString().split('T')[0];
 }
 
-setMaxDate();
+document.addEventListener('DOMContentLoaded', setMaxDate);
 
 document.querySelector('.input-group').addEventListener('submit', submitForm);
 
 function submitForm(event) {
   event.preventDefault();
   deleteInput();
+  if (!checkName()) {
+    showError('Please enter valid name');
+    console.log('ggg');
+    return;
+  }
+  if (!checkAge()) {
+    showError('Please enter valid date of birth');
+    return;
+  }
+
   let userAge = calculateAge();
   showAge(userAge);
-  let daysTillBd = calculateDaysToBirthday();
-  showDaysToBirthday(daysTillBd);
+  let daysTillBirthday = calculateDaysToBirthday();
+  showDaysToBirthday(daysTillBirthday);
 }
 
 function calculateAge() {
   let enteredDate = document.querySelector('.dateofbirth').value;
-  let enteredDateArray = enteredDate.split('-');
+  let userBirthDate = new Date(enteredDate).getTime();
+  let today = new Date().getTime();
 
-  let userBirthDate = new Date(
-    enteredDateArray[0],
-    enteredDateArray[1] - 1,
-    enteredDateArray[2]
-  ).getTime();
-
-  let today = new Date();
-
-  let userFullYears = String(
-    Math.floor((today.getTime() - userBirthDate) / 31557600000)
-  );
+  let userFullYears = String(Math.floor((today - userBirthDate) / 31557600000));
   return userFullYears;
 }
 
 function showAge(userAge) {
+  let userName = getUserName();
   let yearAlert = document.querySelector('.yearsAlert');
   let spanYearsAlert = document.createElement('span');
-  spanYearsAlert.innerHTML = `${getUserName()}, your age is ${userAge} years!`;
+  spanYearsAlert.innerHTML = `${userName}, your age is ${userAge} years!`;
   yearAlert.prepend(spanYearsAlert);
   spanYearsAlert.classList.add('spanYearsAlert');
 }
@@ -60,42 +63,74 @@ function deleteInput() {
 function calculateDaysToBirthday() {
   let today = new Date();
   let enteredDate = document.querySelector('.dateofbirth').value;
-  let enteredDateArray = enteredDate.split('-');
+  let enteredMonth = new Date(enteredDate).getMonth();
+  let enteredDay = new Date(enteredDate).getDate();
 
   let userNextBirthdayThisYear = new Date(
     today.getFullYear(),
-    enteredDateArray[1] - 1,
-    enteredDateArray[2]
+    enteredMonth,
+    enteredDay
   );
   let userNextBirthdayNextYear = new Date(
     today.getFullYear() + 1,
-    enteredDateArray[1] - 1,
-    enteredDateArray[2]
+    enteredMonth,
+    enteredDay
   );
+
   let diffDays = 0;
-  if (enteredDateArray[1] - 1 > today.getMonth()) {
+
+  if (enteredMonth > today.getMonth()) {
     diffDays = Math.floor(
       (userNextBirthdayThisYear.getTime() - today.getTime()) /
-        (1000 * 60 * 60 * 24)
+        (1000 * 60 * 60 * 24) +
+        1
     );
   } else if (
-    enteredDateArray[1] - 1 < today.getMonth() ||
-    (enteredDateArray[1] - 1 == today.getMonth() &&
-      enteredDateArray[2] <= today.getDate())
+    enteredMonth < today.getMonth() ||
+    (enteredMonth == today.getMonth() && enteredDay <= today.getDate())
   ) {
     diffDays = Math.floor(
       (userNextBirthdayNextYear.getTime() - today.getTime()) /
-        (1000 * 60 * 60 * 24)
+        (1000 * 60 * 60 * 24) +
+        1
     );
   }
+
   return diffDays;
 }
-function showDaysToBirthday(daysTillBd) {
+
+function showDaysToBirthday(daysTillBirthday) {
   let daysToBirthdayAlert = document.querySelector('.daysToBirthdayAlert');
   let spanDaysToBirthdayAlert = document.createElement('span');
-  spanDaysToBirthdayAlert.innerHTML = `Your next Birthday is in ${
-    daysTillBd + 1
-  } days.`;
+
+  spanDaysToBirthdayAlert.innerHTML = `Your next Birthday is in ${daysTillBirthday} days.`;
   daysToBirthdayAlert.prepend(spanDaysToBirthdayAlert);
   spanDaysToBirthdayAlert.classList.add('spanDaysToBirthdayAlert');
+}
+
+function checkName() {
+  let userName = getUserName();
+  if (!userName) {
+    return false;
+  } else return true;
+}
+
+function checkAge() {
+  let userAge = calculateAge();
+  if (userAge == 'NaN') {
+    return false;
+  } else return true;
+}
+
+function showError(errorStr) {
+  clearErrorBox();
+  let errorMessage = document.querySelector('.error');
+  let spanErrorMessage = document.createElement('span');
+
+  spanErrorMessage.innerHTML = errorStr;
+  errorMessage.prepend(spanErrorMessage);
+}
+
+function clearErrorBox() {
+  document.querySelector('.error').innerHTML = '';
 }
